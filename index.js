b08290751fc000607708c81ae0c81150e5e55605
Kajal Goel <kajal11122000@gmail.com>
2019-06-15 16:39:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   var i = 0;
+  console.log(cart[i]);
   while(i < cart.length){
     if(cart[i]["itemName"] === item)
     {
